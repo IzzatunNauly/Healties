c939898fdb6bbd5625c9692c29d9d1bc6fbea5bc
IzzatunNauly <naulizzatun159@gmail.com>
2022-06-19 22:39:50 +0700

    menambahkan aut login

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,13 @@
 <?php
 
+use App\Http\Controllers\Admin\DokterController;
+use App\Http\Controllers\Admin\HomeController;
+use App\Http\Controllers\Admin\PasienController;
+use App\Http\Controllers\Admin\PembayaranController;
+use App\Http\Controllers\Admin\RawatInapController;
+use App\Http\Controllers\Admin\RuangController;
+use App\Http\Controllers\Admin\UserController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +22,36 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('login');
+});
+
+Auth::routes(['register' => false]);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware(['auth', 'role'])->group(function(){
+    Route::prefix('admin')->group(function(){
+        Route::controller(HomeController::class)->group(function(){
+            Route::get('home', 'index');
+            Route::get('ubah_profile', 'ubah_profile');
+            Route::put('update_profile', 'update_profile');
+            Route::get('ubah_password', 'ubah_password');
+            Route::put('update_password', 'update_password');
+        });
+
+        Route::resource('ruang', RuangController::class);
+        Route::resource('dokter', DokterController::class);
+        Route::resource('rawat-inap', RawatInapController::class);
+        Route::resource('pasien', PasienController::class);
+        
+        Route::resource('user', UserController::class);
+        Route::get('staff', [UserController::class, 'staff']);
+        
+        Route::controller(PembayaranController::class)->group(function(){
+            Route::get('pembayaran', 'index');
+            Route::get('pembayaran/{id}/create', 'create');
+            Route::post('pembayaran', 'store');
+            Route::get('pembayaran/export', 'export');
+        });
+    });
 });
