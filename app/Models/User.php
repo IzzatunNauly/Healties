5008fb8c466a97b0fd28e3329069a93eeec5d35e
IzzatunNauly <naulizzatun159@gmail.com>
2022-06-19 22:20:11 +0700

    menambahkan model

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,12 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'nama',
         'email',
         'password',
+        'alamat',
+        'role',
+        'foto',
     ];
 
     /**
@@ -41,4 +44,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function pembayaran(){
+        return $this->hasMany(Pembayaran::class);
+    }
 }
